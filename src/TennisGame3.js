1b73674c0841cd54434061c21fbb75c52639ba0e
Maria Soria <maria.it3@gmail.com>
2021-07-05 18:27:00 +0100

    Extract method getFinalScore

--- src/TennisGame3.js
+++ src/TennisGame3.js
@@ -11,20 +11,26 @@ function getPlayerScoreAsText(points) {
   return scoreAsText[points];
 }
 
+function getFinalScore(scorePlayer1, scorePlayer2) {
+  const isTied = scorePlayer1 === scorePlayer2;
+  if (isTied) {
+    return scorePlayer1 + '-All';
+  }
+  return scorePlayer1 + '-' + scorePlayer2;
+}
+
 TennisGame3.prototype.getScore = function () {
-  let scorePlayer1;
   const isTied = this.scorePlayer1 === this.scorePlayer2;
   if ((this.scorePlayer1 < 4 && this.scorePlayer2 < 4) && (this.scorePlayer1 + this.scorePlayer2 < 6)) {
-    scorePlayer1 = getPlayerScoreAsText(this.scorePlayer1);
+    const scorePlayer1 = getPlayerScoreAsText(this.scorePlayer1);
     const scorePlayer2 = getPlayerScoreAsText(this.scorePlayer2);
-    return isTied ? scorePlayer1 + '-All' : scorePlayer1 + '-' + scorePlayer2;
-  } else {
-    if (isTied) {
-      return 'Deuce';
-    }
-    let highestScorePlayer = this.scorePlayer1 > this.scorePlayer2 ? this.namePlayer1 : this.namePlayer2;
-    return ((this.scorePlayer1 - this.scorePlayer2) * (this.scorePlayer1 - this.scorePlayer2) === 1) ? 'Advantage ' + highestScorePlayer : 'Win for ' + highestScorePlayer;
+    return getFinalScore(scorePlayer1, scorePlayer2);
   }
+  if (isTied) {
+    return 'Deuce';
+  }
+  const highestScorePlayer = this.scorePlayer1 > this.scorePlayer2 ? this.namePlayer1 : this.namePlayer2;
+  return ((this.scorePlayer1 - this.scorePlayer2) * (this.scorePlayer1 - this.scorePlayer2) === 1) ? 'Advantage ' + highestScorePlayer : 'Win for ' + highestScorePlayer;
 };
 
 TennisGame3.prototype.wonPoint = function (playerName) {
